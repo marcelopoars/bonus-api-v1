682fa411aeda82d521324e7a1e83c2f70148ea48
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2023-05-24 20:07:34 -0300

    chore: created customers routes

--- app.js
+++ app.js
@@ -4,8 +4,44 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Bonus API...");
+});
+
+// Create customer / POST
+app.post("/customers", (req, res) => {
+  const { name, cpf } = req.body;
+
+  res.send({ name, cpf });
+});
+
+// Get customers / GET
+app.get("/customers", (req, res) => {
+  res.send("Lista de cliente");
+});
+
+// Get by ID / GET
+app.get("/customers/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(id);
+});
+
+// Edit customer by ID / POST
+app.put("/customers/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, cpf } = req.body;
+
+  res.send({ id, name, cpf });
+});
+
+// Delete customer by ID
+app.delete("/customers/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(`Deleted customer by ID: ${id}`);
 });
 
 app.listen(PORT, () => {
