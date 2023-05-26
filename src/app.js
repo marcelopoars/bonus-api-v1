1cf1cb8e7ea3658bb9f8220b6edc957852f3bc7c
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2023-05-25 23:09:32 -0300

    chore: install and configured Prettier and ESLint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
@@ -6,31 +6,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>Bonus API...</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Bonus API...</h1>');
 });
 
 // Create customer / POST
-app.post("/customers", (req, res) => {
+app.post('/customers', (req, res) => {
   const { name, cpf, city, phone } = req.body;
 
   res.send({ id: 1, name, cpf, city, phone, points: 0 });
 });
 
 // Get customers / GET
-app.get("/customers", (req, res) => {
-  res.send("Lista de cliente");
+app.get('/customers', (req, res) => {
+  res.send('Lista de cliente');
 });
 
 // Get customer by ID / GET
-app.get("/customers/:id", (req, res) => {
+app.get('/customers/:id', (req, res) => {
   const { id } = req.params;
 
   res.send(id);
 });
 
 // Edit customer by ID / POST
-app.put("/customers/:id", (req, res) => {
+app.put('/customers/:id', (req, res) => {
   const { id } = req.params;
   const { name, cpf, city, phone, points } = req.body;
 
@@ -38,33 +38,34 @@ app.put("/customers/:id", (req, res) => {
 });
 
 // Delete customer by ID
-app.delete("/customers/:id", (req, res) => {
+app.delete('/customers/:id', (req, res) => {
   const { id } = req.params;
 
   res.send(`Deleted customer with ID ${id}`);
 });
 
 // Create order / POST
-app.post("/orders", (req, res) => {
+app.post('/orders', (req, res) => {
   const { customerId, amount } = req.body;
 
-  const points = Math.ceil(amount / 4)
+  const points = Math.ceil(amount / 4);
 
   res.send({ customerId, amount, points });
 });
 
 // Get orders / GET
-app.get("/orders", (req, res) => {
-  res.send("Lista de vendas");
+app.get('/orders', (req, res) => {
+  res.send('Lista de vendas');
 });
 
 // Get order by ID / GET
-app.get("/orders/:id", (req, res) => {
+app.get('/orders/:id', (req, res) => {
   const { id } = req.params;
 
   res.send(id);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server read on port ${PORT}...`);
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}!🚀\n`);
 });
