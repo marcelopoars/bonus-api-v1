eab1f60e74152d401a1e1db30593869e318fb389
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2023-05-26 18:48:41 -0300

    chore: created JSON response in the routes

--- src/app.js
+++ src/app.js
@@ -7,26 +7,40 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('<h1>Bonus API...</h1>');
+  res.json({
+    name: 'bonus-api-v1',
+    description:
+      'Projeto desenvolvido na disciplina de Desenvolvimento de Serviços e APIs do curso de Análise e Desenvolvimento de Sistemas - Faculdade UniSenac - SENAC-RS.',
+    contributors: [
+      {
+        name: 'Marcelo Pereira',
+      },
+      {
+        name: 'Sohaib Mohammed',
+      },
+    ],
+  });
 });
 
 // Create customer / POST
 app.post('/customers', (req, res) => {
   const { name, cpf, city, phone } = req.body;
 
-  res.send({ id: 1, name, cpf, city, phone, points: 0 });
+  const customer = { id: 1, name, cpf, city, phone, points: 0 };
+
+  res.status(201).json(customer);
 });
 
 // Get customers / GET
 app.get('/customers', (req, res) => {
-  res.send('Lista de cliente');
+  res.json({ data: [] });
 });
 
 // Get customer by ID / GET
 app.get('/customers/:id', (req, res) => {
   const { id } = req.params;
 
-  res.send(id);
+  res.json({ id });
 });
 
 // Edit customer by ID / POST
@@ -34,14 +48,16 @@ app.put('/customers/:id', (req, res) => {
   const { id } = req.params;
   const { name, cpf, city, phone, points } = req.body;
 
-  res.send({ id, name, cpf, city, phone, points });
+  const editedCustomer = { id, name, cpf, city, phone, points };
+
+  res.send(editedCustomer);
 });
 
 // Delete customer by ID
 app.delete('/customers/:id', (req, res) => {
   const { id } = req.params;
 
-  res.send(`Deleted customer with ID ${id}`);
+  res.send({ message: 'Customer deleted', id });
 });
 
 // Create order / POST
@@ -50,19 +66,21 @@ app.post('/orders', (req, res) => {
 
   const points = Math.ceil(amount / 4);
 
-  res.send({ customerId, amount, points });
+  const order = { customerId, amount, points };
+
+  res.status(201).json(order);
 });
 
 // Get orders / GET
 app.get('/orders', (req, res) => {
-  res.send('Lista de vendas');
+  res.json({ data: [] });
 });
 
 // Get order by ID / GET
 app.get('/orders/:id', (req, res) => {
   const { id } = req.params;
 
-  res.send(id);
+  res.json({ id });
 });
 
 app.listen(PORT, () => {
