e6264187322f44b95760804fa7b7498589f9d6f2
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2023-06-08 20:27:12 -0300

    refactor: edited validateIfCustomerExists function

--- src/validations/CustomerValidations/validateIfCustomerExists.js
+++ src/validations/CustomerValidations/validateIfCustomerExists.js
@@ -2,9 +2,9 @@ import { customers } from '../../customers.js';
 import { findOneOnArray } from '../../utils/findOneOnArray.js';
 
 export function validateIfCustomerExists(id) {
-  const customerById = findOneOnArray(id, customers);
+  const customer = findOneOnArray(id, customers);
 
-  if (!customerById) throw { status: 404, message: 'Customer not found' };
+  if (!customer) throw { status: 404, message: 'Customer not found' };
 
-  return customerById;
+  return customer;
 }
